b692b633b7d1a28a4a0befa18b80ac811d76d8e2
piyush025c <17ucs111@lnmiit.ac.in>
2021-02-01 17:44:32 +0530

    Controller and Handler renamed

---
+++ src/main/java/org/example/StudentResourceController.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import dao.StudentDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("students")
+public class StudentResourceController {
+
+
+    static ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
+    static StudentDao studentDao=context.getBean("studentDao",StudentDao.class);
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Student> getStudent()
+    {
+        System.out.println("GET called..");
+        return studentDao.getStudentList();
+    }
+
+    @POST
+    @Path("student")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Student addStudent(Student student)
+    {
+        studentDao.addStudent(student);
+        return student;
+    }
+
+    @PUT
+    @Path("student")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Student updateStudent(Student student)
+    {
+
+         studentDao.updateStudent(student);
+         return student;
+    }
+
+    @DELETE
+    @Path("student/{id}")
+    public void deleteStudent(@PathParam("id") int id)
+    {
+          studentDao.deleteStudent(id);
+    }
+}
